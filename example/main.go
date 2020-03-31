7cdf9393de9e9a5704ca008a78861d9cefbebd9d
Darío <dgrripoll@gmail.com>
2020-03-31 13:25:10 +0200

    Update main.go

--- example/main.go
+++ example/main.go
@@ -9,24 +9,27 @@ import (
 )
 
 func main() {
+	// open the XLSX file.
 	ws, err := xlsx.Open(os.Args[1])
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer ws.Close()
+	defer ws.Close() // do not forget to close
 
+	// iterate over the sheets
 	for _, wb := range ws.Sheets {
 		r, err := wb.Open()
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		for r.Next() {
+		for r.Next() { // get next row
 			fmt.Println(r.Row())
 		}
-		if r.Error() != nil {
+		if r.Error() != nil { // error checking
 			log.Fatalln(r.Error())
 		}
+		// don't forget to close the sheet!!
 		r.Close()
 	}
 }
