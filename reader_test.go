ac1ef251ef2841a86de1906be3bc1c870d5039a3
Dario <dgrripoll@gmail.com>
2020-04-13 13:35:07 +0200

    Removed vim char

--- reader_test.go
+++ reader_test.go
@@ -9,7 +9,7 @@ func TestParseShared(t *testing.T) {
 	sts := []string{
 		"A", "B", "C", "D",
 	}
-	const sharedStringsText = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>^M<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="4" uniqueCount="4"><si><t>A</t></si><si><t>B</t></si><si><t>C</t></si><si><t>D</t></si></sst>`
+	const sharedStringsText = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="4" uniqueCount="4"><si><t>A</t></si><si><t>B</t></si><si><t>C</t></si><si><t>D</t></si></sst>`
 	r := strings.NewReader(sharedStringsText)
 	ss, err := parseShared(r)
 	if err != nil {
@@ -27,7 +27,7 @@ func TestParseSharedWithEmpty(t *testing.T) {
 	sts := []string{
 		"A", "B", "", "C", "", "D",
 	}
-	const sharedStringsText = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>^M<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="4" uniqueCount="4"><si><t>A</t></si><si><t>B</t></si><si><t/></si><si><t>C</t></si><si><t/></si><si><t>D</t></si></sst>`
+	const sharedStringsText = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="4" uniqueCount="4"><si><t>A</t></si><si><t>B</t></si><si><t/></si><si><t>C</t></si><si><t/></si><si><t>D</t></si></sst>`
 	r := strings.NewReader(sharedStringsText)
 	ss, err := parseShared(r)
 	if err != nil {
